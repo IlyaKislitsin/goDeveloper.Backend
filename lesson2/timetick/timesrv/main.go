731ed70810d62f620820e5039c1bd5ccc7cb8fe9
Ilya Kislitsin <ilya@apri-code.ru>
2022-04-03 02:53:07 +0500

    lesson2: added the ability to send messages to clients from the server console

--- lesson2/timetick/timesrv/main.go
+++ lesson2/timetick/timesrv/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
@@ -49,14 +50,55 @@ func main() {
 func handleConn(ctx context.Context, conn net.Conn, wg *sync.WaitGroup) {
 	defer wg.Done()
 	defer conn.Close()
+
+	localWg := &sync.WaitGroup{}
+	localWg.Add(1)
+	go serverWriter(ctx, conn, localWg)
+
 	// каждую 1 секунду отправлять клиентам текущее время сервера
 	tck := time.NewTicker(time.Second)
+
+	for {
+		select {
+		case <-ctx.Done():
+			localWg.Wait()
+			return
+		case t := <-tck.C:
+			fmt.Fprintf(conn, "now: %s\n", t)
+		}
+	}
+}
+
+func serverWriter(ctx context.Context, conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	messages := make(chan string)
+
+	wg.Add(1)
+	go handleServerWriter(ctx, conn, messages, wg)
+
+	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		select {
 		case <-ctx.Done():
 			return
-		case t := <-tck.C:
-			fmt.Fprintf(conn, "now: %s\n", t)
+		default:
+			for scanner.Scan() {
+				messages <- scanner.Text()
+			}
+		}
+	}
+}
+
+func handleServerWriter(ctx context.Context, conn net.Conn, messages <-chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case m := <-messages:
+			fmt.Fprintf(conn, "message from server: %s\n", m)
 		}
 	}
 }
