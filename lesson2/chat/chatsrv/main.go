8b1c1de71520ee48d100bf7d8b0b6f750520105c
Ilya Kislitsin <ilya@apri-code.ru>
2022-04-11 21:55:48 +0500

    lesson2: get client username

--- lesson2/chat/chatsrv/main.go
+++ lesson2/chat/chatsrv/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
 )
 
-type client chan<- string
+type client struct {
+	nickname string
+	messages chan string
+}
 
 var (
 	entering = make(chan client)
@@ -16,62 +24,109 @@ var (
 )
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8001")
+	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt)
+
+	cfg := net.ListenConfig{
+		KeepAlive: time.Minute,
+	}
+
+	listener, err := cfg.Listen(ctx, "tcp", "localhost:8001")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	go broadcaster()
+	wg := &sync.WaitGroup{}
+	log.Println("chat server started!")
+
+	go broadcaster(ctx)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Print(err)
 			continue
 		}
-		go handleConn(conn)
+		wg.Add(1)
+		go handleConn(ctx, conn, wg)
 	}
+
+	<-ctx.Done()
+
+	log.Println("chat server stoping")
+	listener.Close()
+	wg.Wait()
+	log.Println("chat server stoped")
 }
 
-func handleConn(conn net.Conn) {
-	ch := make(chan string)
-	go clientWriter(conn, ch)
+func handleConn(ctx context.Context, conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer conn.Close()
 
-	who := conn.RemoteAddr().String()
-	ch <- "You are " + who
-	messages <- who + " has arrived"
-	entering <- ch
-
-	log.Println(who + " has arrived")
+	nickname := ""
+	fmt.Fprintln(conn, "hello, please write your nickname:")
 
 	input := bufio.NewScanner(conn)
+
 	for input.Scan() {
-		messages <- who + ": " + input.Text()
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			nickname = input.Text()
+			break
+		}
 	}
-	leaving <- ch
-	messages <- who + " has left"
-	conn.Close()
+
+	cli := client{
+		nickname: nickname,
+		messages: make(chan string),
+	}
+
+	go clientWriter(ctx, conn, cli)
+
+	cli.messages <- "You are " + cli.nickname
+	messages <- cli.nickname + " has arrived"
+	entering <- cli
+
+	log.Println(cli.nickname + " has arrived")
+
+	for input.Scan() {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			messages <- cli.nickname + ": " + input.Text()
+		}
+	}
+	leaving <- cli
+	messages <- cli.nickname + " has left"
 }
 
-func clientWriter(conn net.Conn, ch <-chan string) {
-	for msg := range ch {
+func clientWriter(ctx context.Context, conn net.Conn, cli client) {
+	for msg := range cli.messages {
 		fmt.Fprintln(conn, msg)
 	}
 }
 
-func broadcaster() {
+func broadcaster(ctx context.Context) {
 	clients := make(map[client]bool)
 	for {
 		select {
+		case <-ctx.Done():
+			for cli := range clients {
+				delete(clients, cli)
+				close(cli.messages)
+			}
+			return
 		case msg := <-messages:
 			for cli := range clients {
-				cli <- msg
+				cli.messages <- msg
 			}
 		case cli := <-entering:
 			clients[cli] = true
 
 		case cli := <-leaving:
 			delete(clients, cli)
-			close(cli)
+			close(cli.messages)
 		}
 	}
 }
